bb01f0c65b9f1283def8b94918bb6abe9b9f151e
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-02-13 23:11:47 -0800

    Home page is done.

--- react-app/src/pages/Home.jsx
+++ react-app/src/pages/Home.jsx
@@ -10,6 +10,7 @@ import home from "../images/backgrounds/home.png"
 import home_about from "../images/backgrounds/home_about.jpg"
 
 const Home = () => {
+
   let lang = window.location.href
   lang = lang.substr(lang.length-2,2)
   if (lang != "es" && lang != "en") {
@@ -25,29 +26,47 @@ const Home = () => {
         </Slide>
         <img src={minififa} alt="Fifa-certified" className="mini-fifa"/>
       </div>
-        <Slide direction="left" delay="15">
-          <div className="home-section2-container">
-            <img src={logo} alt="TurfMasters" className="home-section2-logo"/>
-              <div className="home-section2-content-container">
-                <h2 className="home-section2-title">{data.home.section2.title[lang]}</h2>
-                <p className="home-section2-content">{data.home.section2.content[lang]}</p>
-              </div>
-          </div>
+      <Slide direction="left" delay="15">
+        <div className="home-section2-container">
+          <img src={logo} alt="TurfMasters" className="home-section2-logo"/>
+            <div className="home-section2-content-container">
+              <h2 className="home-section2-title">{data.home.section2.title[lang]}</h2>
+              <p className="home-section2-content">{data.home.section2.content[lang]}</p>
+            </div>
+        </div>
+      </Slide>
+      <div className="home-section3-container">
+        <img src={home_about} alt="wallpaper" className="home-section3-background" />
+        <Slide direction="left" className="home-section3-content-container">
+          <>
+            <div className="home-section3-text-container">
+              <h2 className="home-section2-title">{data.home.section3.title[lang]}</h2>
+              <p className="home-section2-content">{data.home.section3.content[lang]}</p>
+            </div>
+            <img src={fifa} alt="Fifa-Certificate" className="home-section3-fifa"/>
+            </>
         </Slide>
-          <div className="home-section3-container">
-            <img src={home_about} alt="wallpaper" className="home-section3-background" />
-            <Slide direction="left" className="home-section3-content-container">
-              {/* <div className="home-section3-content-container"> */}
-              <>
-                <div className="home-section3-text-container">
-                  <h2 className="home-section2-title">{data.home.section3.title[lang]}</h2>
-                  <p className="home-section2-content">{data.home.section3.content[lang]}</p>
-                </div>
-                <img src={fifa} alt="Fifa-Certificate" className="home-section3-fifa"/>
-                </>
-              {/* </div> */}
-            </Slide>
-          </div>
+      </div>
+      <div className="home-section4-container">
+        <h1 className="home-section4-title">{data.home.section4.title[lang]}</h1>
+        <div className="home-section4-galery">
+          {
+            data.home.section4.projects.map( (project) => {
+              return (
+                <Link to={`/TurfMasters/projects/${lang}`} className="home-section4-galery-link">
+                  <div className="home-section4-galery-item">
+                    <img src={project["img-url"]} className="home-section4-galery-item-img" alt="hola"/>
+                    <div className="home-section4-galery-item-text-container">
+                      <h2 className="home-section4-galery-item-title">{project.title[lang]}</h2>
+                      <p className="home-section4-galery-item-content">{project.content[lang]}</p>
+                    </div>
+                  </div>
+                </Link>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
