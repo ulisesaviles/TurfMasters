a2baacfb7b35f812c48171010fc47ab9506bac40
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-02-17 13:28:41 -0800

    Installation page is donde

--- react-app/src/pages/Installation.jsx
+++ react-app/src/pages/Installation.jsx
@@ -1,8 +1,29 @@
 import React, { useState } from "react";
+import data from "../text.json";
+import { Slide } from "react-awesome-reveal";
+import contact from "../images/backgrounds/contact.jpg";
+import contact_logo from "../images/backgrounds/instalacion logo.jpg";
 
 const Installation = () => {
+  let lang = window.location.href
+  lang = lang.substr(lang.length-2,2)
+  if (lang != "es" && lang != "en") {
+    lang = "es";
+  }
   return (
-    <h1>Installation</h1>
+    <div className="product-container">
+      <img src={contact} className="products-background" alt="background" />
+      <div className="installation-content-container">
+        <div className="product-right-section-container">
+          <h1 className="installation-title">{data.installation.title[lang]}</h1>
+          <div className="installation-separator"/>
+          <p className="installation-content">{data.installation.content[lang]}</p>
+        </div>
+        <div className="installation-img-container">
+          <img src={contact_logo} className="installation-img" alt="installation-demo"/>
+        </div>
+      </div>
+    </div>
   );
 }
 
